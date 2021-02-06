378eb30e47c735bb23194ce904e3a382e7114964
techasit1589 <techasit.sa@ku.th>
2021-02-06 09:51:00 +0700

    2/6/2021

---
+++ Loop4.py
@@ -0,0 +1,3 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+for key in thisdict.values():
+    print(key)
---
+++ Loop5.py
@@ -0,0 +1,3 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+for key, value in thisdict.items():
+    print(key, value)
